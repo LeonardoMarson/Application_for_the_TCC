bf3e7089ece12477df8c4a0ea5132c73f61b5490
Leonardo Mateus Marson <114182929+LeonardoMarson@users.noreply.github.com>
2025-03-07 14:07:16 -0300

    Update ModelTraining.py

--- ModelTraining.py
+++ ModelTraining.py
@@ -172,44 +172,5 @@ with open('model.tflite', 'wb') as f:
 print("Modelo TFLite salvo como 'model.tflite'")
 
 
-# # Carregando o melhor modelo salvo durante o treinamento ##
-# model = tf.keras.models.load_model('best_model.h5',compile=False)
-# model.export('best_model.h5')
-
-# # A generator that provides a representative dataset
-# def representative_data_gen():
-#   dataset_list = tf.data.Dataset.list_files(caminho_base + '/*/*')
-#   for i in range(100):
-#     image = next(iter(dataset_list))
-#     image = tf.io.read_file(image)
-#     image = tf.io.decode_jpeg(image, channels=3)
-#     image = tf.image.resize(image, [299, 299])
-#     image = tf.cast(image / 255., tf.float32)
-#     image = tf.expand_dims(image, 0)
-#     yield [image]
-
-# # Converter o modelo para TFLite
-# converter = tf.lite.TFLiteConverter.from_saved_model('best_model.h5')
-# converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# converter.representative_dataset = representative_data_gen
-# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-# converter.inference_input_type = tf.uint8
-# converter.inference_output_type = tf.uint8
-# tflite_model = converter.convert()
-# with open('modelF.tflite', 'wb') as file:
-#     file.write(tflite_model)
-
-
-
-
-
-
-# # Salvar o modelo TFLite
-# with open('model.tflite', 'wb') as f:
-#     f.write(tflite_model)
-
-# print("Modelo TFLite salvo como 'model.tflite'")
-
-
 
 
